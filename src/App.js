771631bfbf28b7af81bb614436f91f45d61d38be
Top Deuse <topdeuse@gmail.com>
2019-03-03 19:39:38 +0300

    React-router setup
    - added template for Home component
    - added react-router for navigation between pages

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import './components/Navbar'
+import './components/Navbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Register from './components/Register';
-import Login from './components/Login'
+import Login from './components/Login';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-        <Register />
-        <Login />
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+            <Route exact path="/" component={ Home } />
+            <div className="container">
+              <Route exact path="/register" component={ Register } />
+              <Route exact path="/login" component={ Login } />
+            </div>
+        </div>
+      </Router>
     );
   }
 }
